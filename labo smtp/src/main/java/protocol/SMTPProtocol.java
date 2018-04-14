5ffc1d844a5261d019bc9c237b313afa2619cbaa
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-04-14 11:10:09 +0200

    ajout message supporté par le protocole

--- labo smtp/src/main/java/protocol/SMTPProtocol.java
+++ labo smtp/src/main/java/protocol/SMTPProtocol.java
@@ -1,4 +1,10 @@
 package protocol;
 
 public class SMTPProtocol {
+    public final static String CMD_EHLO = "EHLO ";
+    public final static String CMD_MAIL_FROM = "MAIL FROM: ";
+    public final static String CMD_RCPT_TO = "RCPT TO: ";
+    public final static String CMD_DATA = "DATA\r\n";
+    public final static String CMD_DATA_END = "\r\n.\r\n";
+    public final static String CMD_BYE = "quit\r\n";
 }
