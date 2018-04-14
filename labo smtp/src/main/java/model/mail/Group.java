77194b22b4665ab64a4aaf7845e6fef95fd2fdf0
Florent <shinopill@gmail.com>
2018-04-14 15:43:35 +0200

    model

--- labo smtp/src/main/java/model/mail/Group.java
+++ labo smtp/src/main/java/model/mail/Group.java
@@ -1,4 +1,24 @@
 package model.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author olivier kopp, florent piller
+ */
+
 public class Group {
+    private List<Person> members;
+
+    public Group(){
+        members = new ArrayList<Person>();
+    }
+
+    public void addPerson(Person p){
+        members.add(p);
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
 }
